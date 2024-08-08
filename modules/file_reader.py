5d75cbdda49d8881d43f078415be776b771e7b0c
ranadeep-banik137 <42244847+ranadeep-banik137@users.noreply.github.com>
2024-08-08 20:14:41 +0530

    config path added for local test

--- modules/file_reader.py
+++ modules/file_reader.py
@@ -13,17 +13,20 @@ def get_file_names_from_dir(folder_path=f'{os.getenv("PROJECT_PATH", "")}'):
 def get_json_objects_from_file(json_file_path):
     json_objects = []
     clear_file_data = config['clear_file_after_transfer']
-    with open(json_file_path, 'r+' if clear_file_data else 'r') as json_file:
-        for line in json_file:
-            try:
-                line = line.replace("'", '"')
-                json_obj = json.loads(line)
-                json_objects.append(json_obj)
-            except json.JSONDecodeError as e:
-                logging.error(f'Error in parsing JSON {e}')
-        if clear_file_data:
-            json_file.seek(0)
-            json_file.truncate()
+    try:
+        with open(json_file_path, 'r+' if clear_file_data else 'r') as json_file:
+            for line in json_file:
+                try:
+                    line = line.replace("'", '"')
+                    json_obj = json.loads(line)
+                    json_objects.append(json_obj)
+                except json.JSONDecodeError as e:
+                    logging.error(f'Error in parsing JSON {e}')
+            if clear_file_data:
+                json_file.seek(0)
+                json_file.truncate()
+    except FileNotFoundError as err:
+        logging.error(f"File not found: {json_file_path} {err}")
     # Now `json_objects` contains all the JSON objects from the file
     return json_objects
 
